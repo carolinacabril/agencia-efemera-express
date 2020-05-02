4e07e5f6c9fbe65d3743cb608082a23326f040c2
carolinacabril <carolina-cabril@hotmail.com>
2020-05-02 20:11:35 -0300

    add smooth-scroll in navbar

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// Scroll navbar
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1200,
+    offset: 80
+    });  
+    
+    window.sr = ScrollReveal();
+    sr.reveal('#contato',{duration:2000});
+    sr.reveal('#quem-somos',{duration:2000});
+    sr.reveal('#contato',{duration:2000});
