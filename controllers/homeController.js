ed042c434ad21b4ff7eb01cf5c08d7e652eb3a10
carolinacabril <carolina-cabril@hotmail.com>
2020-05-02 19:25:02 -0300

    Agência Efêmera

---
+++ controllers/homeController.js
@@ -0,0 +1,26 @@
+const homeController = {
+  index: (req, res) => {
+    let carrousel = ['/imagens/image1.jpg', '/imagens/image2.jpg', '/imagens/image3.jpg']
+    let servicos = [
+      {nome: 'Desenvolvimento Web', imagem: '/imagens/undraw_dev_focus.svg'},
+      {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
+      {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'}
+    ]
+    let quemSomos = {
+      nome: 'QUEM SOMOS',
+      descricao: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam eaque, consequatur officia tempora quia harum temporibus voluptatem atque aliquid. Adipisci, debitis tempore optio omnis expedita nisi quam molestias laudantium reiciendis.'
+    }
+    res.render('index', {title: 'Home', listaServicos: servicos, imgsCarrousel: carrousel, sobre: quemSomos})
+  },
+  contato: (req, res) => {
+    let {nome, email, mensagem} = req.body
+
+    res.render('contato', {nome, email, mensagem, title: 'contato'})
+  },
+  newsletter: (req, res) => {
+    let {emailNew} = req.body
+    res.render('newsletter',{title: 'newsletter', emailNew})
+  }
+}
+
+module.exports = homeController;
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/homeController');
+
+/* GET home page. */
+router.get('/', homeController.index);
+router.post('/contato', homeController.contato);
+router.post('/newsletter', homeController.newsletter);
+
+module.exports = router;
